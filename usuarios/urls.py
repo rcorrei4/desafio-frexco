a8ef5b49ec9ca1ff3b57e25b6b22374692af9bd6
rcorrei4 <ricardo.csantos0877@gmail.com>
2022-11-14 14:02:59 -0300

    feat: first push

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastrar/', views.CriarUsuario.as_view(), name='cadastra_usuarios'),
-    path('consulta/', views.ConsultaUsuarios.as_view(), name='consulta_usuarios'),
+    path('cadastrar/usuarios/', views.CriarUsuario.as_view(), name='cadastra_usuarios'),
+    path('consulta/usuarios/', views.ConsultaUsuarios.as_view(), name='consulta_usuarios'),
     path('consulta/usuario/<int:pk>/', views.ConsultaUsuario.as_view(), name='consulta_usuario')
 ]
